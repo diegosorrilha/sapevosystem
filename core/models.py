c2b2d9550e2f25269354e339396e5b8c0570af28
Diego Sorrilha <diego@sorrilha.net>
2018-11-18 18:40:26 -0200

    corrige notnull em models

--- core/models.py
+++ core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Decisor(models.Model):
     projeto = models.ForeignKey('Projeto', on_delete=models.CASCADE, null=True)
     nome = models.CharField(max_length=20)
-    avaliou_criterios = models.BooleanField(default=False)
-    avaliou_alternativas = models.BooleanField(default=False)
+    avaliou_criterios = models.BooleanField(default=False, null=True)
+    avaliou_alternativas = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.nome
