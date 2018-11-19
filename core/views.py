420901718d5699ac6c6e406b6be4c5799b371ec6
Diego Sorrilha <diego@sorrilha.net>
2018-11-19 21:06:28 -0200

    calcula peso das matrizes de criterios, normalizando

--- core/views.py
+++ core/views.py
@@ -265,15 +265,35 @@ def avaliaralternativas(request, projeto_id):
         # avaliacao_alternativas = []
 
 
-
-
-
-
 def _normalizar(lista_elementos):
-    return "teste"
+    lista_final_normalizada = []
+    lista_dos_somados = []
+    lista_normalizada = []
+    lista_sem_zero = []
+    for elemento in lista_elementos:
+        soma = sum(elemento)
+        lista_dos_somados.append(soma)
+    
+    for elemento_da_soma in lista_dos_somados:
+        maior , menor = max(lista_dos_somados), min(lista_dos_somados)
+        if maior == menor:
+            regular = 0
+        else:
+            regular = ((elemento_da_soma - menor)/(maior - menor))
+        lista_normalizada.append(regular)
+    
+    for i in lista_normalizada:
+        if i > 0:
+            lista_sem_zero.append(i)
 
+    for elemento_normalizado in lista_normalizada:
+        if elemento_normalizado > 0:
+            lista_final_normalizada.append(elemento_normalizado)
+        else:
+            menor_zero = min(lista_sem_zero)
+            lista_final_normalizada.append(menor_zero*0.01)
 
-
+    return lista_final_normalizada
 
 
 
@@ -300,7 +320,22 @@ def resultado(request, projeto_id):
         matriz = _gerar_matriz(qtd_criterios, criterios_decisor_1)
         matrizes.append(matriz)
 
-        print(matriz)
+    # calcular pesos dos decisores
+    pesos_decisores = []
+    for matriz in matrizes:
+        peso_matriz = _normalizar(matriz)
+        pesos_decisores.append(peso_matriz)
+
+    for i in pesos_decisores:
+        print(i)
+
+
+    # peso_deciso2 = normalizar(matriz_d_2)
+
+     #peso_final = peso_criterios(peso_deciso1, peso_deciso2)
+
+
+
     
     return render(request, template_name, {
         'projeto_nome': projeto.nome,
